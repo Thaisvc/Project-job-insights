a8ef0847b3e7b290959d4abdac407ecca5e61e8c
thais viana <thaisviana081@.com>
2023-03-01 13:40:03 -0300

    Implemente a função get_unique_job_types

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -15,24 +15,17 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
+    jobs = read(path)
+    # ou  set([job["job_type"] for job in file])
+    job_type_list = set()
+    for type in jobs:
+        job_type_list.add(type["job_type"])
+    return job_type_list
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
+    
     """Filters a list of jobs by job_type
 
     Parameters
