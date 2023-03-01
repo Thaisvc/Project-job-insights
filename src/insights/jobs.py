96c4d31c5ef9c1f0f0d2de7571d024c3334df266
thais viana <thaisviana081@.com>
2023-03-01 13:45:14 -0300

    ajust lint

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -23,7 +23,6 @@ def get_unique_job_types(path: str) -> List[str]:
     return job_type_list
 
 
-
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     
     """Filters a list of jobs by job_type
